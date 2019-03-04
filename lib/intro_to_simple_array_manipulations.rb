a48ae811b2e177c03bb4af7e736e6bc036e48581
Mark Crowder <crowdered@gmail.com>
2019-03-04 02:15:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,48 +16,48 @@ end
  end
  
  def pop_with_args(chars_in_game_of_thrones)
-   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
+   #chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
    element = chars_in_game_of_thrones.pop(2)
  end
  
  def using_shift(my_favorite_cities)
-   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+   #my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
    element = my_favorite_cities.shift
  end
  
  def shift_with_args(ice_cream_brands)
-   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+   #ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
    element = ice_cream_brands.shift(2)
  end
  
  def using_concat(my_favorite_things, all_my_favs)
-   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-   more_favs = ["mario kart", "flatiron school"]
+   #my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+   #more_favs = ["mario kart", "flatiron school"]
    element = my_favorite_things.concat(more_favs)
  end
  
  def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-   list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-   another_esoteric_language = "Malbolge"
+   #list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+   #another_esoteric_language = "Malbolge"
    element = list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
  end
  def using_uniq(captain_planet_and_the_planeteers)
-   captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+   #captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
    element = captain_planet_and_the_planeteers.uniq
  end
  
  def using_flatten(private_colleges_in_newyork)
-   private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+   #private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
    element = private_colleges_in_newyork.flatten
  end
  
  def using_delete(instructors, no_offense_steven)
-   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-   no_offense_steven = "Steven"
+   #instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+   #no_offense_steven = "Steven"
    element = instructors.delete(no_offense_steven)
  end
  
  def using_delete_at(famous_robots, deleted_robot)
-   famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+   #famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
    deleted_robot = using_delete_at.delete(famous_robots,2)
  end
